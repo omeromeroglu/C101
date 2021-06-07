1521c6547e0a8d4da82d98791ff3efc85316e5ee
OMER FARUK OMEROGLU <omeromeroglu95@gmail.com>
2021-06-08 00:37:34 +0300

    Datetime ve Math Sınıfları

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,59 +6,62 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("********* Aritmatik Operatörler **********");
-            int x = 3;
-            int y = 3;
-            y = y + 2;
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(DateTime.Now.Date);
+            Console.WriteLine(DateTime.Now.Day);
+            Console.WriteLine(DateTime.Now.Month);
+            Console.WriteLine(DateTime.Now.Year);
+            Console.WriteLine(DateTime.Now.Hour);
+            Console.WriteLine(DateTime.Now.Minute);
+            Console.WriteLine(DateTime.Now.Second);
 
-            Console.WriteLine(y);
-            y += 2;
-            Console.WriteLine(y);
-            y /= 1;
-            Console.WriteLine(y);
-            y *= 2;
-            Console.WriteLine(y);
+            Console.WriteLine(DateTime.Now.DayOfWeek);
+            Console.WriteLine(DateTime.Now.DayOfYear);
 
-            Console.WriteLine("****** Mantıksal Operatörler *******");
+            Console.WriteLine(DateTime.Now.ToLongDateString());
+            Console.WriteLine(DateTime.Now.ToShortDateString());
+            Console.WriteLine(DateTime.Now.ToLongTimeString());
+            Console.WriteLine(DateTime.Now.ToShortTimeString());
 
-            bool isSuccess = true;
-            bool isCompleted = false;
-
-
-            if (isSuccess && isCompleted)
-                Console.WriteLine("Perfect");
-            if (isSuccess || isCompleted)
-                Console.WriteLine("Good");
-            if (isSuccess && !isCompleted)
-                Console.WriteLine("Fine");
-
-            int a = 3;
-            int b = 4;
-            bool sonuc = a < b;
-
-            Console.WriteLine(sonuc);
-            sonuc = a > b;
-            Console.WriteLine(sonuc);
-            sonuc = a >= b;
-            Console.WriteLine(sonuc);
-            sonuc = a <= b;
-            Console.WriteLine(sonuc);
-            sonuc = a == b;
-            Console.WriteLine(sonuc);
-            sonuc = a != b;
-            Console.WriteLine(sonuc);
+            Console.WriteLine(DateTime.Now.AddDays(2));
+            Console.WriteLine(DateTime.Now.AddHours(3));
+            Console.WriteLine(DateTime.Now.AddSeconds(30));
+            Console.WriteLine(DateTime.Now.AddMonths(2));
+            Console.WriteLine(DateTime.Now.AddYears(22));
+            Console.WriteLine(DateTime.Now.AddMilliseconds(200));
 
 
-            int sayi1 = 10;
-            int sayi2 = 5;
-            int sonuc1 = sayi1 / sayi2;
-            Console.WriteLine(sonuc1);
-            sonuc1 = sayi1 * sayi2;
-            Console.WriteLine(sonuc1);
-            sonuc1 = sayi1 + sayi2;
-            Console.WriteLine(sonuc1);
-            sonuc1 = sayi1++;
-            Console.WriteLine(sonuc1);
+
+            Console.WriteLine(DateTime.Now.ToString("dd"));
+            Console.WriteLine(DateTime.Now.ToString("ddd"));
+            Console.WriteLine(DateTime.Now.ToString("dddd"));
+            Console.WriteLine(DateTime.Now.ToString("MM"));
+            Console.WriteLine(DateTime.Now.ToString("MMM"));
+            Console.WriteLine(DateTime.Now.ToString("MMMM"));
+            Console.WriteLine(DateTime.Now.ToString("yy"));
+            Console.WriteLine(DateTime.Now.ToString("yyyy"));
+
+
+            //MATH
+
+            Console.WriteLine(Math.Abs(-25));
+            Console.WriteLine(Math.Sin(10));
+            Console.WriteLine(Math.Cos(10));
+            Console.WriteLine(Math.Tan(10));
+
+            Console.WriteLine(Math.Ceiling(22.3)); //23
+            Console.WriteLine(Math.Round(22.3));  //22
+            Console.WriteLine(Math.Round(22.7));  //23
+            Console.WriteLine(Math.Floor(22.7)); //22
+
+            Console.WriteLine(Math.Max(2, 6));
+            Console.WriteLine(Math.Min(2, 6));
+
+            Console.WriteLine(Math.Pow(3, 4));
+            Console.WriteLine(Math.Sqrt(9));
+            Console.WriteLine(Math.Log(9));
+            Console.WriteLine(Math.Exp(3));
+            Console.WriteLine(Math.Log10(10));
         }
     }
 }
