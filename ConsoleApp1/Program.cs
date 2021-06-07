f1b07b8822f3a300733b17ed349b698404dc1b4a
OMER FARUK OMEROGLU <omeromeroglu95@gmail.com>
2021-06-08 00:23:49 +0300

    while loop

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,59 +6,13 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("********* Aritmatik Operatörler **********");
-            int x = 3;
-            int y = 3;
-            y = y + 2;
+            Console.Write("**** Foreach ******");
+            string[] arabalar = { "BMW", "Ford", "Toyota", "Nissan" };
 
-            Console.WriteLine(y);
-            y += 2;
-            Console.WriteLine(y);
-            y /= 1;
-            Console.WriteLine(y);
-            y *= 2;
-            Console.WriteLine(y);
-
-            Console.WriteLine("****** Mantıksal Operatörler *******");
-
-            bool isSuccess = true;
-            bool isCompleted = false;
-
-
-            if (isSuccess && isCompleted)
-                Console.WriteLine("Perfect");
-            if (isSuccess || isCompleted)
-                Console.WriteLine("Good");
-            if (isSuccess && !isCompleted)
-                Console.WriteLine("Fine");
-
-            int a = 3;
-            int b = 4;
-            bool sonuc = a < b;
-
-            Console.WriteLine(sonuc);
-            sonuc = a > b;
-            Console.WriteLine(sonuc);
-            sonuc = a >= b;
-            Console.WriteLine(sonuc);
-            sonuc = a <= b;
-            Console.WriteLine(sonuc);
-            sonuc = a == b;
-            Console.WriteLine(sonuc);
-            sonuc = a != b;
-            Console.WriteLine(sonuc);
-
-
-            int sayi1 = 10;
-            int sayi2 = 5;
-            int sonuc1 = sayi1 / sayi2;
-            Console.WriteLine(sonuc1);
-            sonuc1 = sayi1 * sayi2;
-            Console.WriteLine(sonuc1);
-            sonuc1 = sayi1 + sayi2;
-            Console.WriteLine(sonuc1);
-            sonuc1 = sayi1++;
-            Console.WriteLine(sonuc1);
+            foreach (string araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
         }
     }
 }
